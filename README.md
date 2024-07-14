e72deae2ad77c3cc37da13331ba96f045eac68ce
choisauce <austinch20@protonmail.com>
2024-07-13 23:01:37 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# testGit
+
+# Table of Contents
+* [About](#about)
+
+# About
+## testing github
+
+Copyright &copy; 2024 Austin Choi
---
+++ main.py
@@ -0,0 +1,20 @@
+"""
+Copyright (C) 2024 Austin Choi
+See end of file for extended copyright information
+"""
+def main():
+    # Enter code here
+    print("testGit created")
+
+if __name__ == '__main__':
+    main()
+
+"""
+Copyright (C) 2024 Austin Choi
+
+testGit
+testing github
+
+This code is licensed under the GNU General Public License 3.0.
+Please see the LICENSE file in the root directory of this project for the full license details.
+"""
